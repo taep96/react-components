4da82388eb4ba800afb790a1676bd4b37ce6e392
taep96 <64481039+taep96@users.noreply.github.com>
2023-05-21 16:36:43 +0200

    a lot of stuff

---
+++ packages/react-auto-columns/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => ({
+  entry: {
+    index: "src/columns.tsx",
+  },
+  dts: true,
+  format: ["cjs", "esm"],
+  clean: !options.watch,
+  banner: {
+    js: '"use client";',
+  }
+}));
