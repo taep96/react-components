2f68d53feeb705ebe5af258e6fc4a6ad1a4cf916
taep96 <64481039+taep96@users.noreply.github.com>
2023-05-23 00:39:35 +0200

    fix ci typecheck script

--- apps/react-auto-columns-demo/next.config.js
+++ apps/react-auto-columns-demo/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: !!process.env.CI },
+  typescript: { ignoreBuildErrors: !!process.env.CI },
+};
 
 module.exports = nextConfig;
